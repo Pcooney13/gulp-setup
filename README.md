394b42396eaf947467ddaafc94973973692ed7c8
Patrick Cooney <pat.cooney13@gmail.com>
2018-08-08 15:50:31 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # gulp-setup
 
-Basic starter file to get up and running using autoprefixer with latest version of Gulp(4.0)
+need to update gulpfile
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require ('gulp');
 var autoprefixer = require ('gulp-autoprefixer');
 
-gulp.task('styles', function() {
+gulp.task('styles', gulp.series(function(done) {
   gulp.src('css/style.css')
   .pipe(autoprefixer())
   .pipe(gulp.dest('build'))
-});
+  done();
+}));
 
-gulp.task('watch', function() {
-  gulp.watch('css/style.css', ['styles']);
-});
+gulp.task('watch', gulp.series(function() {
+  gulp.watch('css/style.css', gulp.series('styles'));
+}));
