5b2970ff44530c5dc860b66bb565226c583e9718
Pat <pat.cooney13@gmail.com>
2018-08-16 15:47:26 -0400

    added sass, finished armor nav for site

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require ('gulp');
 var autoprefixer = require ('gulp-autoprefixer');
+var sass = require ('gulp-sass');
 
 gulp.task('styles', gulp.series(function(done) {
-  gulp.src('css/style.css')
+  gulp.src('css/style.scss')
+  .pipe(sass({
+    errorLogToConsole: true
+  }))
+  .on ( 'error', console.error.bind(console))
   .pipe(autoprefixer())
   .pipe(gulp.dest('build'))
   done();
 }));
 
 gulp.task('watch', gulp.series(function() {
-  gulp.watch('css/style.css', gulp.series('styles'));
+  gulp.watch('css/*.scss', gulp.series('styles'));
 }));
